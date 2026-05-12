f514cc1b4d6895a8d5465ccb676cc326de178a07
Pham Duy Nguyen <134637351+duynp41@users.noreply.github.com>
2026-05-12 10:15:24 +0700

    Update trigger_dag.sh

--- scripts/trigger_dag.sh
+++ scripts/trigger_dag.sh
@@ -3,12 +3,22 @@ set -e
 
 ENV_NAME="healthcare-airflow"
 LOCATION="asia-southeast1"
-DAG_ID="parent_dag"
 
-echo "--- Run DAG: $DAG_ID ---"
+DAG_IDS=("bigquery_dag" "pyspark_dag" "parent_dag")
 
-gcloud composer environments run $ENV_NAME \
-    --location $LOCATION \
-    dags trigger -- $DAG_ID
+echo "--- TRIGGER DAGS ---"
 
-echo "--- DAG $DAG_ID is triggered ---"
+for DAG_ID in "${DAG_IDS[@]}"
+do
+    echo "--------------------------------------------"
+    echo "Triggering: $DAG_ID..."
+    
+    gcloud composer environments run $ENV_NAME \
+        --location $LOCATION \
+        dags trigger -- "$DAG_ID"
+        
+    echo "Status: DAG $DAG_ID done."
+done
+
+echo "--------------------------------------------"
+echo "--- All DAGs are triggered successfully ---"
